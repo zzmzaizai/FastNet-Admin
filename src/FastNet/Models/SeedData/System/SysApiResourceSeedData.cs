e982c2dd21628feea93f35b28f5859bb84b8448c
张 <zzmzaizai@msn.com>
2023-11-09 08:51:04 +0800

    增加系统种子数据导入

---
+++ src/FastNet/Models/SeedData/System/SysApiResourceSeedData.cs
@@ -0,0 +1,17 @@
+﻿namespace FastNet.Models;
+
+
+/// <summary>
+/// 导入API资源表种子数据
+/// </summary>
+public class SysApiResourceSeedData : ISqlSugarEntitySeedData<SysApiResource>
+{
+    /// <summary>
+    /// 种子数据
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<SysApiResource> SeedData()
+    {
+        return SeedDataUtil.GetSeedData<SysApiResource>("seed_sys_api_resource.json");
+    }
+}
